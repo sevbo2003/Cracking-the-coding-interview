af7c93108724597fa16d8d9a4640de6ced885efd
Rustam-Z <zokirovrustam202@gmail.com>
2022-03-22 00:29:26 +0500

    1.3 and 1.4

--- array_string/ctci_1.4-palindrome-permutations.py
+++ array_string/ctci_1.4-palindrome-permutations.py
@@ -1,5 +1,6 @@
 """
 Palindrome = same backwards and forwards
+Permutation = rearrangement of letters
 
 To be a permutation of a palindrome, a string can have NO MORE THAN one character that is odd.
 For both even and odd cases.
@@ -9,13 +10,15 @@ Solution #1:
 - Then iterate to ensure NO MORE than one char has an odd count
 """
 
+
 def is_permutation_of_palindrome(phrase):
     phrase = phrase.lower().replace(" ", "")
     table = build_char_frequency(phrase)
     return check_max_one_odd(table)
 
+
 def check_max_one_odd(table):
-    found_odd = False # We need just one odd 
+    found_odd = False  # We need just one odd 
     for count in table.values():
         if count % 2 == 1:
             if found_odd:
@@ -23,15 +26,16 @@ def check_max_one_odd(table):
             found_odd = True
     return True
 
+
 def build_char_frequency(phrase):
     table = {}
     for i in phrase:
-        if table.get(i): # Already found this char in string
+        if table.get(i):  # Already found this char in string
             table[i] += 1
         else:
             table[i] = 1
-        
     return table
 
-phrase = "Tact Coa" # 2T, 2A, 2C, O
+
+phrase = "Tact Coa"  # 2T, 2A, 2C, O
 print(is_permutation_of_palindrome(phrase))
