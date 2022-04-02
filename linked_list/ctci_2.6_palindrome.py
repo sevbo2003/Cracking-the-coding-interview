519beab1d742dc61968b1a8c603589cba1a9aef6
Rustam-Z <zokirovrustam202@gmail.com>
2022-04-02 20:53:41 +0500

    2.6 sol 2

--- linked_list/ctci_2.6_palindrome.py
+++ linked_list/ctci_2.6_palindrome.py
@@ -4,11 +4,11 @@ LeetCode: https://leetcode.com/problems/palindrome-linked-list/
 Problem: check is the linked list is palindrome.
 
 Solution:
-0. We will use stack, go till middle by pusing, and pop another middle.
+0. Use stack, go till middle by pushing, and pop another middle.
 1. Use fast and slow pointers. Fast pointer moves 2 steps at a time, slow pointer moves 1 step at a time.
-2. While fast reaches the end, the slow will stand in middle. Push to stack.
-3. Check for even and odd length of LL. If odd fast will be in last element, so move slow to one.
-4. Pop from stack and check the values. Slow ptr is in middle.
+2. While fast reaches the end, the slow will stand in middle.
+3. Check for even and odd length of LL. If odd fast will be in last element (not None), move slow to one (slow = slow.next).
+4. Pop from stack compare with slow.val. Slow ptr is in middle.
 
 Time: O(n)
 Space: O(n)
@@ -41,3 +41,42 @@ class Solution:
             slow = slow.next
             
         return True
+
+
+class Solution2:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head:
+            return None
+        
+        # Let's make a O(1) space algorithm
+        
+        # 1. find middle (slow, fast pointers)
+        # 2. reverse the second half
+        # 3. check palindrome
+        
+        slow, fast = head, head
+        
+        # Find middle
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        # Reverse the second half, 1221 -> 1212
+        prev = None
+        while slow:
+            tmp = slow.next
+            
+            slow.next = prev
+            prev = slow
+            slow = tmp
+            
+        # Check the palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+            
+        return True
+        
