54a644253c00ac119ab8d8fc9cd8fb49e6fc0653
Rustam-Z <zokirovrustam202@gmail.com>
2022-04-21 00:30:05 +0500

    lc 173

---
+++ trees/lc_543_diameter_of_binary_tree.py
@@ -0,0 +1,26 @@
+"""
+LeetCode: 543. Diameter of Binary Tree
+https://leetcode.com/problems/diameter-of-binary-tree/submissions/
+
+Problem: Given the tree, find its diameter. Diameter of the tree, is the longest path between two nodes. If may or may not pass throug root.
+
+             1
+            / \
+           3   2
+         / \
+        4   8
+       /     \
+      6       10
+    /          \
+   8           100
+  /
+ 90
+
+Solution:
+1.
+2.
+3.
+
+Time: O()
+Space: O()
+"""
