f2d2fcc1d89a1b33a55984666cccf5adf202d90e
Rustam-Z <zokirovrustam202@gmail.com>
2022-05-06 23:51:41 +0500

    lc 1209 passed

--- array_string/lc_1209_remove_all_adjacent_duplicates_in_string_ii.py
+++ array_string/lc_1209_remove_all_adjacent_duplicates_in_string_ii.py
@@ -1,6 +1,18 @@
 """
 LeetCode: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+
 Problem: Given the string and k, remove k occurrences of the adjacent characters
+
+Solution:
+1. Use the stack (char, counter), append if chars are not the same, else change counter.
+Time: O(n)
+Space: O(n)
+
+Solution:
+1. Use bool to check if last time we didn't delete anything.
+2. Generally we traverse till the end of string many times till we don't delete anything
+Time: O(??)
+Space: O(1)
 """
 
 
@@ -32,7 +44,24 @@ def removeDuplicates(s: str, k: int) -> str:
     return s
 
 
+def new_solution_remove_duplicates(s: str, k: int) -> str:
+    stack = []
+
+    for c in s:
+        if stack and stack[-1][0] == c:  # check if stack is not empty
+            stack[-1][1] += 1
+
+            if stack[-1][1] == k:
+                stack.pop()
+        else:
+            stack.append([c, 1])
+
+    return ''.join(char * count for char, count in stack)
+
+
 if __name__ == '__main__':
     s = "deeedbbcccbdaa"
     k = 3
     print(removeDuplicates(s, k))
+    print(new_solution_remove_duplicates(s, k))
+
