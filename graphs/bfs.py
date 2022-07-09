8abba4c71aa8149f09710409c108397fbbd52b87
Rustam-Z <zokirovrustam202@gmail.com>
2022-07-09 21:59:23 +0500

    724. Find Pivot Index

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,23 +1,27 @@
-graph = {
-    '5': ['3', '7'],
-    '3': ['2', '4'],
-    '7': ['8'],
-    '2': [],
-    '4': ['8'],
-    '8': []
-}
+"""
+Breadth First Search
+    - Starts at any node and explores ALL neighbours first, before moving to the next level of neighbours.
 
-visited = []  # List for visited nodes
-queue = [] 
+Application of BFS Algorithm
+    - The Shortest path on unweighted graph
+
+Time complexity: O(V+E), number vertices and edges
+Space complexity: O(V)
+
+https://www.youtube.com/watch?v=oDqjPvD54Ss
+https://www.programiz.com/dsa/graph-bfs
+"""
 
 
 def bfs(visited, graph, node):
-    visited.append(node)
+    visited = [node]  # List for visited nodes
+    queue = []
+
     queue.append(node)
 
-    while queue:  
-        m = queue.pop(0) 
-        print(m, end=" ") 
+    while queue:
+        m = queue.pop(0)
+        print(m, end=" ")
 
         for neighbor in graph[m]:
             if neighbor not in visited:
@@ -26,4 +30,13 @@ def bfs(visited, graph, node):
 
 
 if __name__ == "__main__":
-    bfs(visited, graph, '5')  
+    graph = {
+        '5': ['3', '7'],
+        '3': ['2', '4'],
+        '7': ['8'],
+        '2': [],
+        '4': ['8'],
+        '8': []
+    }
+
+    bfs(graph, '5')
--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,22 +1,45 @@
-graph = {
-    '5': ['3', '7'],
-    '3': ['2', '4'],
-    '7': ['8'],
-    '2': [],
-    '4': ['8'],
-    '8': []
-}
+"""
+Depth First Search
+    - Starts at any node and plunges a depth first into a graph (like a snake), until it cannot go any further
+    - At point it can't go any further it backtracks and continues
 
-visited = set()  # Set to keep track of visited nodes of graph.
+Application of DFS Algorithm
+    - For finding the path
+    - To test if the graph is bipartite
+    - For finding the strongly connected components of a graph
+    - For detecting cycles in a graph
+
+Time complexity: O(V+E), number vertices and edges
+Space complexity: O(V)
+
+https://www.youtube.com/watch?v=7fujbpJ0LB4
+https://www.programiz.com/dsa/graph-dfs
+"""
 
 
-def dfs(visited, graph, node): 
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+
     if node not in visited:
-        print(node, end=" ")
+        # print(node, end=" ")
+        traversal.append(node)
         visited.add(node)
-        for neighbor in graph[node]:
-            dfs(visited, graph, neighbor)
+
+        for neighbour in graph[node]:  # graph[node] - visited
+            dfs(graph, neighbour, visited)
 
 
 if __name__ == "__main__":
-    dfs(visited, graph, '5')
+    traversal = []
+    graph = {
+        '5': ['3', '7'],
+        '3': ['2', '4'],
+        '7': ['8'],
+        '2': [],
+        '4': ['8'],
+        '8': [],
+    }
+
+    dfs(graph, '5')
+    print(traversal)
