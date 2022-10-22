0245ba8101ba7667b144b71a243fa00781e4af8e
Rustam-Z <zokirovrustam202@gmail.com>
2022-10-22 13:06:36 +0500

    Patching in testing updated

--- sandbox.py
+++ sandbox.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 
 def my_func():
-    response_data = requests.get("https://google.com").text
+    response_data = requests.get("https://jsonplaceholder.typicode.com/todos/1").text
     response_data_upper = response_data.upper()
     print(response_data_upper)
     return response_data_upper
@@ -12,11 +12,14 @@ def my_func():
 my_func()
 
 
-@patch('sandbox.requests.get')
-def test_my_func(mock_write):
+@patch(f'{__name__}.requests.get')
+def check_my_func(mock_write):
     mock_write.return_value.text = "rustam"
     # Use side_effect if there are multiple similar function with different arguments
     # https://stackoverflow.com/questions/68075655/python-mock-patch-two-functions-that-are-similar
     x = my_func()
     assert x == "RUSTAM"
 
+
+check_my_func()
+my_func()
