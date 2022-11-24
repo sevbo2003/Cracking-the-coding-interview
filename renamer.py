daab1b0a3b7076b6a27972c941a43c0c5b97da72
Rustam-Z <zokirovrustam202@gmail.com>
2022-11-24 17:39:14 +0500

    refactored renamer func

--- renamer.py
+++ renamer.py
@@ -8,14 +8,14 @@ def process_string(input_file_name):
     Function to take the string and return the processed string, to use in file names.
 
     :param input_file_name: 1480. Running Sum of 1d Array
-    :return: slc_1480_running_sum_of_1d_array.py
+    :return: lc_running_sum_of_1d_array.py
     """
     file_name_lower_letters = input_file_name.lower()
     split_on_point = file_name_lower_letters.rsplit(". ")
     number = split_on_point[0]
     only_letters = split_on_point[1].split()
     underscored_file_name = "_".join(only_letters)
-    joined_file_name = PREFIX + number + "_" + underscored_file_name + ".py"
+    joined_file_name = PREFIX + underscored_file_name + ".py"
     return joined_file_name
 
 
