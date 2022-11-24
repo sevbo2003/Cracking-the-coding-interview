119a53e4665111c48446222c62ac75825441ce94
Rustam-Z <zokirovrustam202@gmail.com>
2022-11-24 20:43:12 +0500

    Meeting rooms II

--- renamer.py
+++ renamer.py
@@ -3,25 +3,28 @@ import subprocess
 PREFIX = 'lc_'
 
 
-def process_string(input_file_name):
+def process_string(input_file_name: str, difficulty_level: str) -> str:
     """
-    Function to take the string and return the processed string, to use in file names.
+    Generates the proper naming for files.
 
+    Example usage:
     :param input_file_name: 1480. Running Sum of 1d Array
-    :return: lc_running_sum_of_1d_array.py
+    :param difficulty_level: e
+    :return: lc_e_running_sum_of_1d_array.py
     """
+
     file_name_lower_letters = input_file_name.lower()
     split_on_point = file_name_lower_letters.rsplit(". ")
-    number = split_on_point[0]
     only_letters = split_on_point[1].split()
     underscored_file_name = "_".join(only_letters)
-    joined_file_name = PREFIX + underscored_file_name + ".py"
+    joined_file_name = PREFIX + difficulty_level.lower() + '_' + underscored_file_name + ".py"
     return joined_file_name
 
 
 def main():
     file_name_input = input("Your file name: ")
-    joined_file_name = process_string(file_name_input)
+    difficulty_level = input("Difficulty leve (E, M, H): ")
+    joined_file_name = process_string(file_name_input, difficulty_level)
     print(joined_file_name)
     subprocess.run("pbcopy", universal_newlines=True, input=joined_file_name)  # Copy to clipboard.
 
